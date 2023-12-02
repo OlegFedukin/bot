84e15c12820de81c7820e8173659b4f7e2ce17b6
OlegFedukin <119093952+OlegFedukin@users.noreply.github.com>
2023-12-02 14:02:12 +0600

    Update tests.py

--- bot/shop/tests.py
+++ bot/shop/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Games
 
-# Create your tests here.
+class GamesTestCase(TestCase):
+    def test_model_name(self):
+        game1 = Games.objects.create(name="tigr", price=34)
+        self.assertEqual(game1.name, "tigr")
+
+    def test_model_price(self):
+        game1 = Games.objects.create(name="tigr", price=676)
+        self.assertEqual(game1.price, 676)
